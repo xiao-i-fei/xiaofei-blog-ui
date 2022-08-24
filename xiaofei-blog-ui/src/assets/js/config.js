345ce8c399120f6eee1b46504015efdf3b4e7c80
LiFei <1903078434@qq.com>
2022-08-24 19:09:06 +0800

    qq和微博登录配置

--- xiaofei-blog-ui/src/assets/js/config.js
+++ xiaofei-blog-ui/src/assets/js/config.js
@@ -1,7 +1,7 @@
 export default {
-    QQ_APP_ID: "101878726",
-    QQ_REDIRECT_URI: "https://www.talkxj.com/oauth/login/qq",
-    WEIBO_APP_ID: "4039197195",
-    WEIBO_REDIRECT_URI: "https://www.talkxj.com/oauth/login/weibo",
+    QQ_APP_ID: "102017575",
+    QQ_REDIRECT_URI: "https://www.naste.top/oauth/login/qq",
+    WEIBO_APP_ID: "326658755",
+    WEIBO_REDIRECT_URI: "https%3A%2F%2Fwww.naste.top%2Foauth%2Flogin%2Fweibo",
     TENCENT_CAPTCHA: "2088053498"
 };
